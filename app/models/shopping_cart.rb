8d92c1cf5d2fc357a0667b8fb09812a5258e46cd
Zong Han <zonghangoh@gmail.com>
2023-12-06 22:52:23 +0800

    shopping cart, add/remove items, total cost

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,20 @@
 class ShoppingCart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
+
+  def add_item(item)
+    cart_items.find_or_create_by(item: item).increment(:quantity)
+  end
+
+  def remove_item(item)
+    cart_item = cart_items.find_by(item: item)
+    if cart_item&.quantity >= 1
+      cart_item.decrement(:quantity)
+      cart_item.destroy if cart_item.quantity.zero?
+    end
+  end
+
+  def total_cost
+    cart_items.sum { |cart_item| cart_item.quantity * cart_item.item.price }
+  end
 end
