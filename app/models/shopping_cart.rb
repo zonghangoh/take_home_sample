240ef9152f5c569757a38dcfbaa7e1e8f9d3ab4b
Zong Han <zonghangoh@gmail.com>
2023-12-07 09:17:05 +0800

    Update shopping_cart.rb

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -19,6 +19,6 @@ class ShoppingCart < ApplicationRecord
   end
 
   def total_cost
-    cart_items.sum { |cart_item| cart_item.quantity * cart_item.item.price }
+    includes(cart_items: :item).cart_items.sum { |cart_item| cart_item.quantity * cart_item.item.price }
   end
 end
